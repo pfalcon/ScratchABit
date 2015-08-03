f8de675318b8ef23cb5c5ca5014af5c9d12cc743
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-08-04 00:07:08 +0300

    scratchabit: Don't crash if there's an error processing user command.
    Instead, log exception, show dialog with it to a user, suggesting to save
    teh database and quit, and continue.

--- scratchabit.py
+++ scratchabit.py
@@ -180,6 +180,31 @@ class Editor(editor.EditorExt):
         self.show_status("Analyzing (%d insts so far)" % cnt)
 
     def handle_key(self, key):
+        try:
+            return self.handle_key_unprotected(key)
+        except Exception as e:
+            log.exception("Exception processing user command")
+            L = 5
+            T = 2
+            W = 70
+            H = 20
+            self.dialog_box(L, T, W, H)
+            v = Viewer(L + 1, T + 1, W - 2, H - 2)
+            import traceback
+            v.set_lines([
+                "Exception occured processing the command. Press Esc to continue.",
+                "Recommended action is saving database, quitting and comparing",
+                "database files with backup copies for possibility of data loss",
+                "or corruption. The exception was also logged to scratchabit.log.",
+                "Please report way to reproduce it to",
+                "https://github.com/pfalcon/ScratchABit/issues",
+                "",
+            ] + traceback.format_exc().splitlines())
+            v.loop()
+            self.update_screen()
+
+
+    def handle_key_unprotected(self, key):
         if key == editor.KEY_ENTER:
             line = self.get_cur_line()
             log.info("Enter pressed: %s" % line)
