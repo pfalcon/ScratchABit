36b845bf0978258f068537f8a4f9180041577e9b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-08-22 00:44:28 +0300

    saveload: Save and load new YAML-based format, migrate old format if found.

--- saveload.py
+++ saveload.py
@@ -1,3 +1,4 @@
+import sys
 import os
 
 import engine
@@ -10,31 +11,21 @@ def save_exists(project_dir):
 def save_state(project_dir):
     if not os.path.isdir(project_dir):
         os.makedirs(project_dir)
-    files = ["project.labels", "project.comments", "project.args",
-             "project.xrefs", "project.funcs", "project.aspace"]
+    files = ["project.aspace", "project.aprops"]
     for fname in files:
         if os.path.exists(project_dir + "/" + fname):
             os.rename(project_dir + "/" + fname, project_dir + "/" + fname + ".bak")
-    with open(project_dir + "/project.labels", "w") as f:
-        engine.ADDRESS_SPACE.save_labels(f)
-    with open(project_dir + "/project.comments", "w") as f:
-        engine.ADDRESS_SPACE.save_comments(f)
-    with open(project_dir + "/project.args", "w") as f:
-        engine.ADDRESS_SPACE.save_arg_props(f)
-    with open(project_dir + "/project.xrefs", "w") as f:
-        engine.ADDRESS_SPACE.save_xrefs(f)
-    with open(project_dir + "/project.funcs", "w") as f:
-        engine.ADDRESS_SPACE.save_funcs(f)
+
     with open(project_dir + "/project.aspace", "w") as f:
         engine.ADDRESS_SPACE.save_areas(f)
-    with open(project_dir + "/project.yaml-ref", "w") as f:
-        engine.ADDRESS_SPACE.save_ref_yaml(f)
     with open(project_dir + "/project.aprops", "w") as f:
         engine.ADDRESS_SPACE.save_addr_props(f)
 
 
-def load_state(project_dir):
-    print("Loading state...")
+def migrate_to_yaml(project_dir):
+    print("Migrating saved state to YAML-based format...")
+    with open(project_dir + "/project.aspace", "r") as f:
+        engine.ADDRESS_SPACE.load_areas(f)
     with open(project_dir + "/project.labels", "r") as f:
         engine.ADDRESS_SPACE.load_labels(f)
     with open(project_dir + "/project.comments", "r") as f:
@@ -45,6 +36,25 @@ def load_state(project_dir):
         engine.ADDRESS_SPACE.load_xrefs(f)
     with open(project_dir + "/project.funcs", "r") as f:
         engine.ADDRESS_SPACE.load_funcs(f)
+
+    with open(project_dir + "/project.aprops", "w") as f:
+        engine.ADDRESS_SPACE.save_ref_yaml(f)
+
+    if not os.path.isdir(project_dir + "/old"):
+        os.makedirs(project_dir + "/old")
+    files = ["project.labels", "project.comments", "project.args",
+             "project.xrefs", "project.funcs"]
+    for fname in files:
+        os.rename(project_dir + "/" + fname, project_dir + "/old/" + fname)
+    print("Migration complete. Restart to use new saved state format.")
+    sys.exit(100)
+
+
+def load_state(project_dir):
+    if not os.path.exists(project_dir + "/project.aprops"):
+        migrate_to_yaml(project_dir)
+
+    print("Loading state...")
     with open(project_dir + "/project.aspace", "r") as f:
         engine.ADDRESS_SPACE.load_areas(f)
     with open(project_dir + "/project.aprops", "r") as f:
