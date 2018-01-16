3dcb3e3baad0e7276c5343adc8710373278eb073
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-17 00:25:55 +0200

    default_plugins: Use universal arm_32_capstone plugin for arm_32.

--- default_plugins.py
+++ default_plugins.py
@@ -2,8 +2,7 @@ loaders = ["elf"]
 
 cpus = {
 #"arm_32_thumb": "arm_thumb",  # Warning: this supports only Thumb, not Thumb2
-"arm_32_thumb": "arm_32_thumb_capstone",
-"arm_32_arm": "arm_32_arm_capstone",
+"arm_32": "arm_32_capstone",
 "x86_32": "x86_32_pymsasid",
 "x86_64": "x86_64_pymsasid",
 "xtensa_32": "xtensa",
