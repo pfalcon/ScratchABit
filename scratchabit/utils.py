36a662fcbcec13aaf667149caf06c137e18eae32
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-20 12:02:58 +0200

    utils: Add "$" to the set of "word" chars.
    Commonly used for special symbols, e.g. "$a__6".

--- scratchabit/utils.py
+++ scratchabit/utils.py
@@ -13,7 +13,7 @@ def get_word_at_pos(str, pos):
         return None
     if pos >= len(str):
         pos = len(str) - 1
-    word_chars = string.ascii_letters + string.digits + "._+"
+    word_chars = string.ascii_letters + string.digits + "$._+"
     if str[pos] not in word_chars:
         return None
     beg = pos
